724ae493f258d24f217c345e9c993e4f449c3d5b
Hernani Pawlick David <hernanipawlick@yahoo.com.br>
2019-12-08 16:24:37 +0000

    Added plugin project

---
+++ src/pa/iscde/tasklist/TaskListView.java
@@ -0,0 +1,30 @@
+package pa.iscde.tasklist;
+
+
+import java.util.Map;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import pt.iscte.pidesco.extensibility.PidescoView;
+
+public class TaskListView implements PidescoView {
+
+	public TaskListView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
+		viewArea.setLayout(new RowLayout(SWT.HORIZONTAL));
+		Label label = new Label(viewArea, SWT.NONE);
+		label.setImage(imageMap.get("tasklist.png"));
+		Text text = new Text(viewArea, SWT.BORDER);
+		text.setText("Task List");
+	}
+
+}
